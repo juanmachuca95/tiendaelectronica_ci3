69cd8de02a06758f5f713d7642ec70d4fc7b4e77
Machuca Juan <machucajuangabriel@gmail.com>
2021-06-13 20:13:21 -0300

    update

--- application/views/comercializacion/index.php
+++ application/views/comercializacion/index.php
@@ -2,8 +2,8 @@
     <div class="row m-0">
         <div class="col-md-12 m-0 p-0">
             <div class="">
-                <div class="container p-0">
-                    <h2 class="display-4 p-5">Comercializaci&oacuten</h2>
+                <div class="container p-0 py-5">
+                    <h2 class="display-4">Comercializaci&oacuten <i class="fas fa-shopping-cart"></i></h2>
                     <p class="lead px-3">
                         Souvenirs ZN le provee al usuario muchisimas herramientas de interaccion, con la empresa.
                         Whattsapp, email, o simplemente a traves de la pagina web.
@@ -13,7 +13,7 @@
                         Otra forma de obtener tu producto es a traves de pedidos que lo hacen en la misma pagina. Importante que empezar a hacer pedido tenes que <strong>Registrate para poder Usar el Panel de Pedidos.</strong>
                     </p>
 
-                    <h2 class="p-3">Medios de Pago</h2>
+                    <h2 class="p-3 font-weight-light">Medios de Pago</h2>
                     <p class="lead px-3">
 
                         Ahora comprar es más sencillo, tenemos medios de pago presenciales a través de nuestras sucursales en la provincia de Corrientes
@@ -24,23 +24,36 @@
                     <p class="lead px-3">
                         También te ofrecemos medios de pago electronico y contra envio a disposición del cliente, para que puedas realizar la compra que necesitas
                         sin moverte de tu casa.
+
+                        El usuario tiene a disposición el uso de la compra directa (NO REQUIERE REGISTRARSE EN SITIO WEB) o compras a tráves de la habilitación de carritos (REQUIERE REGISTRACIÓN PREVIA EN LA APLICACIÓN).            
+
+                        A fin de proporcionar las mejores herramientas al usuario para la interacción dentro de nuestro sitio web, le brindamos como usuario todas las posibilidades de compra disponibles. Estás modos de compra están divididos en dos secciones:  
+                        <ul class="lead"> GESTIÓN DE PAGOS ELECTRÓNICOS
+                            <li>Tarjeta de credito</li>
+                            <li>Tarjeta de debito</li>
+                            <li>Pago en efectivo - Cupon de pago</li>
+                            <li>PagoFácil</li>
+                            <li>Rapipago</li>
+                        </ul>
+
+                        <ul class="lead"> PAGOS CONTRA ENVIO
+                            <li>Pagos contra entrega.</li>
+                        </ul>
+                    </p>
+                    <p class="lead">
+                        En nuestro la gestión de pagos electrónicos se realizan con la ayuda de MercadoPago, plataforma lider en pagos electrónicos. La cual permite todas las sitadas maneras de pago a fin de acomodar los medios de pago a la conveniencia del usuario.
+                        Por otro lado si el usuario no utiliza MercadoPago o por otras razones no utiliza ninguna de las anteriores formas de pago. El usuario está libre de realizar las compras abonando el costo de envio del producto al momento de recibir su compra en su propio domicilio haciendo uso de la opción pagos contra entrega.
                     </p>
 
-                    <div class="row  d-flex justify-content-center mb-5">
-                        <div class="col-12 col-md-3">
-                            <img class="img-fluid" src="<?= base_url('assets/img/mediospagos/mercadopago.png') ?>" alt="mercadopago">
-                        </div>
-                        <div class="col-12 col-md-3">
-                            <img class="img-fluid" src="<?= base_url('assets/img/mediospagos/mastercard.png') ?>" alt="mastercard">
-                        </div>
-                        <div class="col-12 col-md-3">
-                            <img class="img-fluid" src="<?= base_url('assets/img/mediospagos/naranja.jpg') ?>" alt="naranja">
+                    <div class="row  d-flex justify-content-center align-items-center my-5">
+                        <div class="col-12 col-md-8">
+                            <img class="img-fluid w-100 rounded" src="<?= base_url('assets/img/mediospagos/mediospagos.png') ?>" alt="mercadopago">
                         </div>
                     </div>
 
 
                     <h2 class="p-3">Tipos de Entrega e información adicional</h2>
-                    <p class="lead font-weight-bold px-3">
+                    <p class="lead font-weight-light px-3">
 
                         Una vez confirmado tu pedido nos pondremos en contacto contigo para que nos podamos facilitar
                         los datos para realizar el envio, dependiendo de la zona geográfica tu producto llega en un tiempo estimado de 5 a 10 días. <br>
