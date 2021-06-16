d253d024588f3e67e0027f361b04584526afa63c
Machuca Juan <machucajuangabriel@gmail.com>
2021-06-16 13:11:14 -0300

    update

--- application/views/layout/nav_cliente.php
+++
@@ -1,6 +0,0 @@
-
-<div class="container p-0 mt-4">
-    <nav class="nav nav-pills flex-column flex-sm-row bg-dark">
-        <a class="font-weight-bold flex-sm-fill text-sm-center nav-link text-warning" href="<?=base_url('cliente/pedidosConfirmados')?>">Panel de Control de Pedidos</a>
-    </nav>
-</div>  
